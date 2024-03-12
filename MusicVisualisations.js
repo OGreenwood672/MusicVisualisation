122498c6ba8740c69bd325657c034c961a9b29ab
Greenwood <greenwood672.dev@gmail.com>
2024-03-12 18:34:12 +0000

    oops

--- MusicVisualisations.js
+++ MusicVisualisations.js
@@ -109,7 +109,7 @@ function draw() {
         updatePosition(deltaTime / 1000, platforms);
 
         if (
-            platforms[platforms.length - 1].x < WIDTH /// 2 - PLATFORMWIDTH
+            platforms[platforms.length - 1].x < WIDTH / 2 - PLATFORMWIDTH
         ) {
             platform = platforms[platforms.length - 1].generateNext(song_beats[song_beat_index], totalx);
             song_beat_index++;
